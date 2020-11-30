88bbe6014d15b27b0f4cbd8926657d5ca8c2f336
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-11-30 20:12:25 -0300

    Keys

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         'tFVV5AeSH9odelRAJuFdWMXUU',//process.env.key,
-  consumer_secret:      'AtKXwvde9Xn2ad7bgKa5hisONX69jVxYmMBAn7nD6Fepqr71jK', //process.env.secret,
-  access_token:         '1150956918137131008-hek73JzLdkvlpP1UGiuapevEwZhF2z', //process.env.token,
-  access_token_secret:  '1alTr46mOfBkQUo9YZt0iCYr6dPnjFznb3shlEYJdboFO', //process.env.token_secret,
+  consumer_key:         process.env.key,
+  consumer_secret:      process.env.secret,
+  access_token:         process.env.token,
+  access_token_secret:  process.env.token_secret,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
