9c3c2f56fa533cf6f5ca1c1c302f6c577e55ebd7
Maximo Signiorini <maximosigniorini97@gmail.com>
2020-12-01 12:19:22 -0300

    Update config

--- config.js
+++ config.js
@@ -1,12 +1,3 @@
-// module.exports = {
-//   consumer_key:         process.env.key,
-//   consumer_secret:      process.env.secret,
-//   access_token:         process.env.token,
-//   access_token_secret:  process.env.token_secret,
-//   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-//   strictSSL:            true,     // optional - requires SSL certificates to be valid.
-// }
-
 module.exports = {
   consumer_key:         process.env.API_KEY,
   consumer_secret:      process.env.API_SECRET_KEY,
